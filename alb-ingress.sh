f9afa3f1ab00753e7cd6652294d1c14df5bfa967
PsychicRam <60787030+ramprasad9840@users.noreply.github.com>
2025-12-08 19:48:24 +0530

    Update alb-ingress.sh

--- alb-ingress.sh
+++ alb-ingress.sh
@@ -1,1 +1,13 @@
 
+CLUSTER_NAME=<your-cluster-name>
+REGION=<your-region>   # e.g. ap-south-1
+
+OIDC_URL=$(aws eks describe-cluster \
+  --name "$CLUSTER_NAME" \
+  --region "$REGION" \
+  --query "cluster.identity.oidc.issuer" \
+  --output text)
+
+echo "OIDC_URL = $OIDC_URL"
+
+curl -I "$OIDC_URL"
